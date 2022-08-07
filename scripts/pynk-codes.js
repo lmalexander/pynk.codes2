3d2208f604ec4f501a7b28bec861b8b90d8df1e1
EMA <elizabeth.alexander@northwestern.edu>
2022-08-07 16:19:05 -0400

    added rawData json to dataBtn() with pynk song and lyric information

--- scripts/pynk-codes.js
+++ scripts/pynk-codes.js
@@ -31,9 +31,113 @@ console.log("down in the valley where the girls get naked")
         document.getElementById("dcViewer").innerHTML = dcViewerSrc;
     };
 
-/* >>> data
+
+
+    /* data button function
+    - returns seven lines of pynk json object as unprocessed data to the dataViewer container.
+    - this function is connected to the first button on the page, #btn-data  */
+    
+    
+    function dataBtn() {
+        // check on-click event + function is working
+        console.log("data-Btn onclick worked!")
+
+        let rawData = {
+                        "1" : {
+                            "songData" : {
+                                "title" : "PYNK",
+                                "artist" : "Janelle Monae ft. Grimes",
+                                "album" : "'Dirty Computer'",
+                                "year" : "2018",
+                                "lyricsData" : [
+                                    "the inside of your...baby", 
+                                    "behind all of the doors...crazy",
+                                    "the tongue that goes down...maybe",
+                                    "the paradise found",
+                                    "when you're blushing inside...baby",
+                                    "the truth you can't hide...maybe",
+                                    "the folds of your brain...crazy",
+                                    "as we all go insane",
+                                    "the lips around your...maybe",
+                                    "the skin that's under...baby",
+                                    "where it's deepest inside...crazy",
+                                    "beyond forest and thighs",
+                                    "the secrets you hide...maybe",
+                                    "the lid of your eye...baby",
+                                    "where all of it starts...crazy",
+                                    "the holes of your heart",
+                                    "the inside of your...baby (we're all just pynk)",
+                                    "the walls and the doors...maybe (deep inside, we're all just pynk)",
+                                    "your fingers in my...maybe",
+                                    "the truth you can't hide",
+                                    "your tongue going round...baby",
+                                    "the sun going down...maybe",
+                                    "the holes in your heart...baby",
+                                    "my favorite part",
+                                    ]
+                                },
+                            },
+                        "2": {
+                            "songData" : {
+                                "title": "Down In The Valley",
+                                "artist": "Jucee Froot",
+                                "album": "'P-Valley: Season 1 (Music from the Original TV Series)'",
+                                "year" : "2020",
+                                "lyricsData" : [
+                                    "down in the valley",
+                                    "where the girls get naked",
+                                    "if you throwin' bands",
+                                    "then you know she gon shake it",
+                                    "one, two, break 'em",
+                                    "three, four, rake 'em",
+                                    "these niggas grind hard",
+                                    "but these bitches grind harder",
+                                    "climbing up the pole",
+                                    "just to get out the bottom",
+                                    "the crowd, below",
+                                    "stay ready for the show",
+                                    "the pimps, the dough",
+                                    "don't let it take your soul",
+                                    "we make fallin' out the sky look easy",
+                                    "look at my bitches",
+                                    "gangsta walkin' on the ceiling",
+                                    "green on the floor",
+                                    "money talk, can you hear me?",
+                                    "the world is addicting",
+                                    "but dreams is expensive",
+                                    "keep spending",
+                                    "work hard for the top",
+                                    "can't fall now",
+                                    "but whatever goes up",
+                                    "must fall down",
+                                    "what you do",
+                                    "when the power out",
+                                    "kids hungry",
+                                    "can't do nothing but scream shout",
+                                    "we all choose to live",
+                                    "but it's different routes",
+                                    "take the shit with the shine",
+                                    "gotta make it count",
+                                    "when you live and die",
+                                    "by the paper route",
+                                    "spend a dime",
+                                    "just to make it out, yeah",
+                                    ]
+                                },
+                            }
+                        }
+    };
+
+    /* pynk button function
+    - returns seven lines of pynk json object structured as a seven line poetic stanza to the pynkViewer container.
+    - this function is connected to the first button on the page, #btn-data  */
+    function pynkBtn() {
+        // check on-click event + function is working
+        console.log("pynk-Btn onclick worked!")
+
+        /* >>> data
     This object stores all of the phrases used in the song "PYNK" to describe Pynk, organized by verse. */
-    let pynk = {
+    let pynkData = {
         "pynk" : [
             "the inside of your...baby", 
             "behind all of the doors...crazy",
@@ -101,21 +205,6 @@ console.log("down in the valley where the girls get naked")
             "just to make it out, yeah"
         ]           
     };
-
-    /* data button function
-    - returns seven lines of pynk json object as unprocessed data to the dataViewer container.
-    - this function is connected to the first button on the page, #btn-data  */
-    function dataBtn() {
-        // check on-click event + function is working
-        console.log("data-Btn onclick worked!")
-    };
-
-    /* pynk button function
-    - returns seven lines of pynk json object structured as a seven line poetic stanza to the pynkViewer container.
-    - this function is connected to the first button on the page, #btn-data  */
-    function pynkBtn() {
-        // check on-click event + function is working
-        console.log("pynk-Btn onclick worked!")
 
     };
 
