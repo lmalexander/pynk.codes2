525fbf0efcfeca6bd8a67732d115a3cbfbee3afa
EMA <elizabeth.alexander@northwestern.edu>
2022-08-08 17:15:03 -0400

    able to return random song data to the page table! still need to fix the button function to create 7 table lines on one press and not duplicate the header row.

--- scripts/pynk-codes.js
+++ scripts/pynk-codes.js
@@ -1,5 +1,93 @@
 console.log("down in the valley where the girls get naked")
 
+// >>>>> ALL OF THE DATA.
+
+// rawData JSON
+let rawData = {
+    "0" : {
+        "songData" : {
+            "title" : "'PYNK'",
+            "artist" : "Janelle Monae ft. Grimes",
+            "album" : "'Dirty Computer'",
+            "year" : "2018",
+            "lyricsData" : [
+                "the inside of your...baby", 
+                "behind all of the doors...crazy",
+                "the tongue that goes down...maybe",
+                "the paradise found",
+                "when you're blushing inside...baby",
+                "the truth you can't hide...maybe",
+                "the folds of your brain...crazy",
+                "as we all go insane",
+                "the lips around your...maybe",
+                "the skin that's under...baby",
+                "where it's deepest inside...crazy",
+                "beyond forest and thighs",
+                "the secrets you hide...maybe",
+                "the lid of your eye...baby",
+                "where all of it starts...crazy",
+                "the holes of your heart",
+                "the inside of your...baby (we're all just pynk)",
+                "the walls and the doors...maybe (deep inside, we're all just pynk)",
+                "your fingers in my...maybe",
+                "the truth you can't hide",
+                "your tongue going round...baby",
+                "the sun going down...maybe",
+                "the holes in your heart...baby",
+                "my favorite part",
+                ]
+            },
+        },
+    "1": {
+        "songData" : {
+            "title": "'Down In The Valley'",
+            "artist": "Jucee Froot",
+            "album": "'P-Valley: Season 1 (Music from the Original TV Series)'",
+            "year" : "2020",
+            "lyricsData" : [
+                "down in the valley",
+                "where the girls get naked",
+                "if you throwin' bands",
+                "then you know she gon shake it",
+                "one, two, break 'em",
+                "three, four, rake 'em",
+                "these niggas grind hard",
+                "but these bitches grind harder",
+                "climbing up the pole",
+                "just to get out the bottom",
+                "the crowd, below",
+                "stay ready for the show",
+                "the pimps, the dough",
+                "don't let it take your soul",
+                "we make fallin' out the sky look easy",
+                "look at my bitches",
+                "gangsta walkin' on the ceiling",
+                "green on the floor",
+                "money talk, can you hear me?",
+                "the world is addicting",
+                "but dreams is expensive",
+                "keep spending",
+                "work hard for the top",
+                "can't fall now",
+                "but whatever goes up",
+                "must fall down",
+                "what you do",
+                "when the power out",
+                "kids hungry",
+                "can't do nothing but scream shout",
+                "we all choose to live",
+                "but it's different routes",
+                "take the shit with the shine",
+                "gotta make it count",
+                "when you live and die",
+                "by the paper route",
+                "spend a dime",
+                "just to make it out, yeah",
+                ]
+            },
+        }
+    };
+
 /* >>> dirty computer viewer
     returns a random dirty computer headshot to the viewer
     next - clears data narrative viewer on click */
@@ -38,95 +126,8 @@ console.log("down in the valley where the girls get naked")
     - returns seven lines of pynk json object as unprocessed data to the dataViewer container.
     - this function is connected to the first button on the page, #btn-data */
     function dataBtn() {
-
         // check on-click event + function is working
         console.log("data-Btn onclick worked!");
-
-        // rawData json object
-        let rawData = {
-            "1" : {
-                "songData" : {
-                    "title" : "PYNK",
-                    "artist" : "Janelle Monae ft. Grimes",
-                    "album" : "'Dirty Computer'",
-                    "year" : "2018",
-                    "lyricsData" : [
-                        "the inside of your...baby", 
-                        "behind all of the doors...crazy",
-                        "the tongue that goes down...maybe",
-                        "the paradise found",
-                        "when you're blushing inside...baby",
-                        "the truth you can't hide...maybe",
-                        "the folds of your brain...crazy",
-                        "as we all go insane",
-                        "the lips around your...maybe",
-                        "the skin that's under...baby",
-                        "where it's deepest inside...crazy",
-                        "beyond forest and thighs",
-                        "the secrets you hide...maybe",
-                        "the lid of your eye...baby",
-                        "where all of it starts...crazy",
-                        "the holes of your heart",
-                        "the inside of your...baby (we're all just pynk)",
-                        "the walls and the doors...maybe (deep inside, we're all just pynk)",
-                        "your fingers in my...maybe",
-                        "the truth you can't hide",
-                        "your tongue going round...baby",
-                        "the sun going down...maybe",
-                        "the holes in your heart...baby",
-                        "my favorite part",
-                        ]
-                    },
-                },
-            "2": {
-                "songData" : {
-                    "title": "Down In The Valley",
-                    "artist": "Jucee Froot",
-                    "album": "'P-Valley: Season 1 (Music from the Original TV Series)'",
-                    "year" : "2020",
-                    "lyricsData" : [
-                        "down in the valley",
-                        "where the girls get naked",
-                        "if you throwin' bands",
-                        "then you know she gon shake it",
-                        "one, two, break 'em",
-                        "three, four, rake 'em",
-                        "these niggas grind hard",
-                        "but these bitches grind harder",
-                        "climbing up the pole",
-                        "just to get out the bottom",
-                        "the crowd, below",
-                        "stay ready for the show",
-                        "the pimps, the dough",
-                        "don't let it take your soul",
-                        "we make fallin' out the sky look easy",
-                        "look at my bitches",
-                        "gangsta walkin' on the ceiling",
-                        "green on the floor",
-                        "money talk, can you hear me?",
-                        "the world is addicting",
-                        "but dreams is expensive",
-                        "keep spending",
-                        "work hard for the top",
-                        "can't fall now",
-                        "but whatever goes up",
-                        "must fall down",
-                        "what you do",
-                        "when the power out",
-                        "kids hungry",
-                        "can't do nothing but scream shout",
-                        "we all choose to live",
-                        "but it's different routes",
-                        "take the shit with the shine",
-                        "gotta make it count",
-                        "when you live and die",
-                        "by the paper route",
-                        "spend a dime",
-                        "just to make it out, yeah",
-                        ]
-                    },
-                }
-            };
 
     /* rawDataTable -- table that will be dynamically appended to page on button click */
 
@@ -141,7 +142,7 @@ console.log("down in the valley where the girls get naked")
         // >>>>> 2. CREATE TABLE HEADER
 
         // table header array
-        let rawDataTableHeaders = [ "Phrase", "Song Title", "Artist", "Album", "Year"];
+        let rawDataTableHeaders = [ "Lyric", "Song Title", "Artist", "Album", "Year"];
 
         // create table head element
         let rawDataTableHead = document.createElement("thead");
@@ -150,7 +151,7 @@ console.log("down in the valley where the girls get naked")
         let rawDataTableHeaderRow = document.createElement("tr");
 
         // assign table header row class
-        rawDataTableHeaderRow.className = "raw-data-table-header-row";
+        // rawDataTableHeaderRow.className = "raw-data-table-header-row";
 
         // check that header array is populated
         console.log(rawDataTableHeaders);
@@ -182,24 +183,108 @@ console.log("down in the valley where the girls get naked")
         rawDataTableHead.append(rawDataTableHeaderRow);
 
         // check that table head element is populated with table header row
-         console.log(rawDataTableHead)
+        console.log(rawDataTableHead);
 
-        // append table head element to table element
-        document.getElementById("raw-data-table-head").append(rawDataTableHead) 
+        // append table head element to table element on page
+        document.getElementById("raw-data-table-head").append(rawDataTableHead); 
         
         // >>>>> 3. CREATE TABLE BODY
-        
-        // create table body element
-        //let rawDataTableBody = document.createElement("tbody");
-        
-        // assign table body element class
-        //rawDataTableBody.className = "raw-data-table-body";
 
-        // append table body element to table
-        //rawDataTable.append(rawDataTableBody);
+        // >>> 1. create table elements
+        // create rawDataTable body element
+        let rawDataTableBody = document.createElement("tbody");
 
-        // return rawDataTable to page
-        //document.getElementById("raw-data-table").append = rawDataTable;
+        // return rawDataTable body element to page
+        document.getElementById("raw-data-table-body").append(rawDataTableBody);
+
+        // create rawDataTable row element
+        let rawDataTableRow = document.createElement("tr");
+
+        // create rawDataTableRow cell elements
+        let rawDataTableLyricCell = document.createElement("td");
+        let rawDataTableSongTitleCell = document.createElement("td");
+        let rawDataTableArtistCell = document.createElement("td");
+        let rawDataTableAlbumCell = document.createElement("td");
+        let rawDataTableYearCell = document.createElement("td");
+
+        // >>> 2. create table data variables
+        // create rawDataTable variables
+        let rawDataTableRowData = "";
+        let rawDataTableLyric = "";
+        let rawDataTableSongTitle = "";
+        let rawDataTableArtist = "";
+        let rawDataTableAlbum = "";
+        let rawDataTableYear = ""
+
+        // >>> 3. return data to variables
+        // random rawData index number
+        let rawDataIndex = Math.floor(Math.random() * 2);
+
+        // check rawData index number
+        console.log(rawDataIndex);
+
+        // random rawData lyric index number
+        let rawDataLyricIndex = Math.floor(Math.random() * rawData[0].songData.lyricsData.length);
+
+        // check rawData lyric index number
+        console.log(rawDataLyricIndex);
+
+        // select lyric from rawData based on random index number
+        rawDataTableLyric = rawData[rawDataIndex].songData.lyricsData[rawDataLyricIndex];
+
+        // check rawDataTableLyric
+        console.log(rawDataTableLyric);
+
+        // insert rawDataTableLyric into rawDataTableLyricCell
+        rawDataTableLyricCell.innerText = rawDataTableLyric;
+
+        // check rawDataTableLyricCell
+        console.log(rawDataTableLyricCell);
+
+        // save song title of selected lyric to rawDataTableSongTitle
+        rawDataTableSongTitle = rawData[rawDataIndex].songData.title;
+
+        // check correct song title data was pulled
+        console.log(rawDataTableSongTitle);
+
+        // insert rawDataTableSongTitle into rawDataTableSongTitleCell
+        rawDataTableSongTitleCell.innerText = rawDataTableSongTitle;
+    
+        // save artist of selected lyric to rawDataTableArtist
+        rawDataTableArtist = rawData[rawDataIndex].songData.artist;
+
+        // check correct song title data was pulled
+        console.log(rawDataTableArtist);
+
+        // insert rawDataTableArtist into rawDataTableSongArtistCell
+        rawDataTableArtistCell.innerText = rawDataTableArtist;
+
+        // save album of selected lyric to rawDataTableAlbum
+        rawDataTableAlbum = rawData[rawDataIndex].songData.album;
+
+        // check correct song title data was pulled
+        console.log(rawDataTableAlbum);
+
+        // insert rawDataTableYear into rawDataTableSongYearCell
+        rawDataTableAlbumCell.innerText = rawDataTableAlbum;
+
+        // save year of selected lyric to rawDataTableYear
+        rawDataTableYear = rawData[rawDataIndex].songData.year;
+
+        // check correct song title data was pulled
+        console.log(rawDataTableYear);
+
+        // insert rawDataTableYear into rawDataTableSongYearCell
+        rawDataTableYearCell.innerText = rawDataTableYear;
+
+        // append rawDataTable cells to rawDataTableRow
+        rawDataTableRow.append(rawDataTableLyricCell, rawDataTableSongTitleCell, rawDataTableArtistCell, rawDataTableAlbumCell, rawDataTableYearCell);
+
+        // check that row was populated
+        console.log(rawDataTableRow);
+
+        // add row to table body
+        rawDataTableBody.appendChild(rawDataTableRow);
 
 
 
